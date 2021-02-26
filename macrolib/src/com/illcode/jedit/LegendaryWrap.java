af25f982fec14d5dfb92598cac95a867bd41060a
Jesse Pavel <jpavel@alum.mit.edu>
2021-02-26 14:28:59 -0500

    Building up LegendaryWrap

--- macrolib/src/com/illcode/jedit/LegendaryWrap.java
+++ macrolib/src/com/illcode/jedit/LegendaryWrap.java
@@ -1,9 +1,7 @@
 package com.illcode.jedit;
 
 import org.gjt.sp.jedit.Buffer;
-import org.gjt.sp.jedit.EditPane;
 import org.gjt.sp.jedit.View;
-import org.gjt.sp.jedit.gui.DockableWindowManager;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
 
 import org.apache.commons.lang3.StringUtils;
@@ -17,36 +15,30 @@ public class LegendaryWrap
 {
     // MACRO START
 
-    /**
-     * Test if a character represents one of the punctuation characters that Pandoc Markdown
-     * uses to indicate a list of some sort.
-     */
-    boolean isListPunctuation(char c) {
-        return (c == '*' || c == '+' || c == '-' || c == ':' || c == '~' || c == '#');
-    }
-
-    Pattern NUMERIC_LIST_ITEM_PATTERN = Pattern.compile("[\\d#]\\.\\s");
+    Pattern LIST_ITEM_PATTERN = Pattern.compile(" *(?:[\\*\\+\\-:~]|(?:\\d+|#)\\.) +");
 
     /**
-     * Tests if a string indicates the start of an ordered list
+     * Tests if a line is the start of some sort of list item, and if so, return the text
+     * up until the end of the start pattern; otherwise return {@code null}.
      */
-    boolean isOrderedListItem(String s) {
-        Matcher m = NUMERIC_LIST_ITEM_PATTERN.matcher(s);
-        return m.lookingAt();
+    String getListStartText(String s) {
+        Matcher m = LIST_ITEM_PATTERN.matcher(s);
+        if (m.lookingAt())
+            return m.group();
+        else
+            return null;
     }
 
     /**
-     * Tests if a line is the start of some sort of list item.
+     * Returns the leading whitespace of a string.
      */
-    boolean isListStart(String s) {
-        return isListPunctuation(s.charAt(0)) || isOrderedListItem(s);
-    }
-
-    /**
-     * Checks if a line represents a paragraph boundary.
-     */
-    boolean isLineParagraphBoundary(String line) {
-        return StringUtils.isWhitespace(line) || isListStart(line);
+    String getLeadingWhitespace(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isWhitespace(s.charAt(i)))
+                return s.substring(0, i);
+        }
+        // If we reach here, the string was all whitespace
+        return s;
     }
 
     /**
@@ -61,14 +53,63 @@ public class LegendaryWrap
      */
     Object[] getParagraphText(JEditTextArea textArea) {
         Object[] oa = new Object[3];
+        int parStartLine, parEndLine;
+        String parText;
 
+        int numLines = textArea.getLineCount();
+        int currentLine = textArea.getCaretLine();
+
+        // If the current line is all whitespace, there's nothing to be done
+        if (StringUtils.isWhitespace(textArea.getLineText(currentLine))) {
+            oa[0] = "";
+            oa[1] = oa[2] = currentLine;
+            return oa;
+        }
+
+        if (numLines == 1) {  // degenerate case
+            oa[0] = textArea.getBuffer().getText();
+            oa[1] = oa[2] = 0;
+            return oa;
+        }
+
+        // First we scan up looking for the paragraph start line
+        for (parStartLine = currentLine; parStartLine >= 0; parStartLine--) {
+            String lineText = textArea.getLineText(parStartLine);
+            if (getListStartText(lineText) != null) {
+                break;
+            } else if (StringUtils.isWhitespace(lineText)) {
+                parStartLine++;   // the previously-scanned line is the paragraph start
+                break;
+            }
+        }
+        parStartLine = Math.max(parStartLine, 0);
+        // Now scan down looking for the paragraph end line
+        for (parEndLine = currentLine + 1; parEndLine < numLines; parEndLine++) {
+            String lineText = textArea.getLineText(parEndLine);
+            if (getListStartText(lineText) != null || StringUtils.isWhitespace(lineText)) {
+                parEndLine--; // the previously-scanned line is the paragraph end
+                break;
+            }
+        }
+        parEndLine = Math.min(parEndLine, numLines - 1);
+        int startOffset = textArea.getLineStartOffset(parStartLine);
+        int endOffset = textArea.getLineEndOffset(parEndLine);
+        // getLineEndOffset() always returns an offset 1 past the end of the
+        // line's (non-newline) text, so we subtract 1 below
+        parText = textArea.getText(startOffset, endOffset - startOffset - 1);
+        oa[0] = parText;
+        oa[1] = parStartLine;
+        oa[2] = parEndLine;
         return oa;
+    }
+
+    void reflowText(String parText, StringBuilder sb, int wrapMargin) {
     }
 
     /**
      * This is the "main" method of the macro
      */
-    void formatParagraph(JEditTextArea textArea, View view) {
+    void formatParagraph(View view, JEditTextArea textArea, Buffer buffer) {
         int parStartLine = -1, parEndLine = -1;
         String parText;
         boolean usingSelection;
@@ -84,14 +125,23 @@ public class LegendaryWrap
             parStartLine = (Integer) parInfo[1];
             parEndLine = (Integer) parInfo[2];
         }
+        if (StringUtils.isWhitespace(parText))
+            return;
+
         trailingNewline = parText.endsWith("\n");
+        int tabSize = buffer.getIntegerProperty("tabSize", 8);
+        parText.replace("\t", StringUtils.repeat(' ', tabSize));
 
         StringBuilder sb = new StringBuilder(parText.length() + 100);
+
+        reflowText(parText, sb, buffer.getIntegerProperty("maxLineLen", 0));
 
         // Save our reformatted text back to parText
         if (trailingNewline)
             sb.append('\n');
         parText = sb.toString();
+
+        int caretPos = textArea.getCaretPosition();
 
         // Finally, modify the text of the buffer
         if (usingSelection) {
@@ -102,6 +152,8 @@ public class LegendaryWrap
             textArea.setSelection(new Selection.Range(startOffset, endOffset));
             textArea.setSelectedText(parText);
         }
+
+        textArea.setCaretPosition(Math.min(caretPos, buffer.getLength()));
     }
 
     // markdownFormatParagraph();
