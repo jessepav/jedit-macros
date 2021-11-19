3524841f1ecd9cfd39dcb6a3c7f28bf667cb78f5
Jesse Pavel <jpavel@alum.mit.edu>
2021-11-19 10:56:07 -0500

    Working on SwitchBuffer

--- macrolib/src/com/illcode/jedit/SwitchBuffer.java
+++ macrolib/src/com/illcode/jedit/SwitchBuffer.java
@@ -2,6 +2,11 @@ package com.illcode.jedit;
 
 import com.jgoodies.forms.factories.CC;
 import com.jgoodies.forms.layout.FormLayout;
+import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.gui.DockableWindowManager;
+import org.gjt.sp.jedit.textarea.JEditTextArea;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -9,23 +14,29 @@ import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import java.awt.Container;
 import java.awt.Font;
-import java.awt.Frame;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class SwitchBuffer implements KeyListener
 {
+    Buffer buffer;
+    View view;
+    EditPane editPane;
+    JEditTextArea textArea;
+    DockableWindowManager wm;
+    String scriptPath;
+
     JDialog dialog;
     JFrame owner;
     JLabel label;
-    JTextArea textArea;
+    JTextArea bufferTextArea;
 
     void initComponents(JFrame owner) {
         this.owner = owner;
 
         dialog = new JDialog(owner, true);
         label = new JLabel();
-        textArea = new JTextArea();
+        bufferTextArea = new JTextArea();
 
         //======== dialog ========
         dialog.setTitle("Switch Buffer");
@@ -36,26 +47,33 @@ public class SwitchBuffer implements KeyListener
 
         //---- label ----
         label.setText("Choose Buffer:");
+        label.setFocusable(false);
         contentPane.add(label, CC.xy(2, 2));
 
         //---- textArea ----
-        textArea.setEditable(false);
-        textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
-        textArea.setOpaque(false);
-        contentPane.add(textArea, CC.xy(2, 4));
+        bufferTextArea.setEditable(false);
+        bufferTextArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        bufferTextArea.setOpaque(false);
+        contentPane.add(bufferTextArea, CC.xy(2, 4));
     }
 
     void show() {
+        StringBuilder sb = new StringBuilder(256);
+
+        bufferTextArea.setText(sb.toString());
+        sb = null;
+
         dialog.pack();
         dialog.setLocationRelativeTo(owner);
-        textArea.addKeyListener(this);
-        textArea.requestFocusInWindow();
+        bufferTextArea.addKeyListener(this);
+        bufferTextArea.requestFocusInWindow();
         dialog.setVisible(true);  // blocks until hidden
-        textArea.removeKeyListener(this);
+        bufferTextArea.removeKeyListener(this);
     }
 
     public void keyTyped(KeyEvent e) {
         char c = e.getKeyChar();
+        c = Character.toUpperCase(c);
 
         dialog.setVisible(false);
     }
